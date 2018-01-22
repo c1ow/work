ca129137e7f287df99e5211c22fd5b6388c82814
c1ow <1127366104@qq.com>
2018-01-22 21:01:26 +0800

    ll

--- work-spark/src/main/java/com/spark/SparkTest.java
+++ work-spark/src/main/java/com/spark/SparkTest.java
@@ -1,5 +1,36 @@
 package com.spark;
 
-public class SparkTest {
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+/**
+ * spark 测试
+ * @author 11273
+ *
+ */
+public class SparkTest {
+	public static void getCount() throws InterruptedException {
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.setAppName("work count");
+		sparkConf.setMaster("spark://192.168.0.120:4040");
+		JavaStreamingContext jsc = new JavaStreamingContext(sparkConf,Durations.seconds(1));
+		JavaReceiverInputDStream<String> lines = jsc.socketTextStream("192.168.0.120", 4040);
+		JavaDStream<String> errorLines = lines.filter(new Function<String, Boolean>() {
+			
+			@Override
+			public Boolean call(String arg0) throws Exception {
+				return arg0.contains("error");
+			}
+		});
+		errorLines.print();
+		jsc.start();
+		jsc.awaitTermination();
+	}
+	public static void main(String[] args) throws InterruptedException {
+		getCount();
+	}
 }
